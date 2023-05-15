3ebec53627fb69a60094063c8ab0913b3995aa00
sacam5 <130437587+sacam5@users.noreply.github.com>
2023-05-15 07:48:31 +0200

    Update report.php

--- report.php
+++ report.php
@@ -6,9 +6,9 @@
 </head>
 <body>
   <h2>Aliens Abducted Me - Report an Abduction</h2>
-
 <?php
-  $name = $_POST['firstname'] . ' ' . $_POST['lastname'];
+  $first_name = $_POST['firstname'];
+  $last_name = $_POST['lastname'];
   $when_it_happened = $_POST['whenithappened'];
   $how_long = $_POST['howlong'];
   $how_many = $_POST['howmany'];
@@ -18,15 +18,18 @@
   $email = $_POST['email'];
   $other = $_POST['other'];
 
-  $to = 'sacaraskovic5@gmail.com';
-  $subject = 'Aliens Abducted Me - Abduction Report';
-  $msg = "$name was abducted $when_it_happened and was gone for $how_long.\n" .
-    "Number of aliens: $how_many\n" .
-    "Alien description: $alien_description\n" .
-    "What they did: $what_they_did\n" .
-    "Fang spotted: $fang_spotted\n" .
-    "Other comments: $other";
-  mail($to, $subject, $msg, 'From:' . $email);
+  $dbc = mysqli_connect( `sql7.freesqldatabase.com', 'sql7617235', 'E5LQZ7cMvB', 'aliens_abduction')
+    or die('Error connecting to MySQL server.');
+
+  $query = "INSERT INTO aliens_abduction (first_name, last_name, when_it_happened, how_long, " .
+    "how_many, alien_description, what_they_did, fang_spotted, other, email) " .
+    "VALUES ('$first_name', '$last_name', '$when_it_happened', '$how_long', '$how_many', " .
+    "'$alien_description', '$what_they_did', '$fang_spotted', '$other', '$email')";
+
+  $result = mysqli_query($dbc, $query)
+    or die('Error querying database.');
+
+  mysqli_close($dbc);
 
   echo 'Thanks for submitting the form.<br />';
   echo 'You were abducted ' . $when_it_happened;
